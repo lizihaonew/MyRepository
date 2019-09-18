f36e8a1a6d9e25319ea6069d505ff1e2fa4733a5
lizihaonew <1287626466@qq.com>
2019-09-18 23:03:33 +0800

    mock test

---
+++ www/ImoocInterface/case/test_mock.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author   : Lizihao
+# @Time     : 2019/9/4 21:33
+# @File     : demo.py
+
+import json
+import time
+import unittest
+from www.ImoocInterface.base.demo import RunMain
+from HTMLTestRunner import HTMLTestRunner
+from mock import mock
+from www.ImoocInterface.base.mock_demo import mock_test
+
+
+class TestMethod(unittest.TestCase):
+
+    def setUp(self):
+        self.run = RunMain()
+
+    def test_01(self):
+        url = 'http://www.imooc.com/m/web/shizhanapi/loadmorepingjia.html'
+        data = {
+            'cart': '11'
+        }
+        res = self.run.run_main(url, 'POST', data)
+        self.assertEqual(res['code'], 200)
+        globals()['userId'] = 2003
+        print '这是我的第一个case'
+
+    def test_02(self):
+        # print userId
+        url = 'http://www.imooc.com/m/web/shizhanapi/loadmorepingjia.html'
+        data = {
+            'cart': '12'
+        }
+        mock_data = {
+            'username': 'xiaolizi',
+            'password': '1234567',
+            'code': 200
+        }
+        self.run.run_main = mock.Mock(return_value=mock_data)
+        res = self.run.run_main(url, 'POST', data)
+        print res
+        self.assertEqual(res['code'], 200)
+
+    def test_03(self):
+        url = 'http://www.imooc.com/m/web/shizhanapi/loadmorepingjia.html'
+        data = {
+            'cart': '12'
+        }
+        mock_data = {
+            'username': 'xiaolizi',
+            'password': '1234567',
+            'code': 200
+        }
+        res = mock_test(self.run.run_main,url,'POST',data,mock_data)
+        print res
+        self.assertEqual(res['code'], 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
