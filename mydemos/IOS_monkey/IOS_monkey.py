807ff0d977fd5cb57613ed55cdf0f78a3e9b830e
lizihao <lizihao@dangdang.com>
2021-01-04 16:07:46 +0800

    add send_email

--- mydemos/IOS_monkey/IOS_monkey.py
+++ mydemos/IOS_monkey/IOS_monkey.py
@@ -9,6 +9,11 @@ from appium import webdriver
 from random import randint
 import os
 import configparser
+import requests
+from get_ios_log import get_log_file, analyse_log_file
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
 
 config = configparser.ConfigParser()
 config.read('config.conf', encoding='utf-8-sig')
@@ -41,7 +46,7 @@ class IOSMonkey:
         desired_caps['bundleId'] = bundle_id
         desired_caps['platformVersion'] = '11.0'
         desired_caps["udid"] = device_udid
-        desired_caps["xcodeOrgId"] = "ZTW9XPA927"
+        desired_caps["xcodeOrgId"] = "7KM2N7UNX3"
         desired_caps["xcodeSigningId"] = "iPhone Developer"
         desired_caps["udid"] = self.udid
 
@@ -74,14 +79,14 @@ class IOSMonkey:
         return dir_path
 
     def random_tap(self):
-        if self.driver is not None:
+        if self.driver:
             win_size = self.driver.get_window_size()
             x = randint(1, win_size["width"])
             y = randint(5, win_size["height"] - 5)
             self.driver.tap([(x, y)])
 
     def swipe_y(self):
-        if self.driver is not None:
+        if self.driver:
             win_size = self.driver.get_window_size()
             x = win_size["width"] / 2
             start_y = randint(30, win_size["height"])
@@ -95,7 +100,7 @@ class IOSMonkey:
                 duration=1000)
 
     def swipe_x(self):
-        if self.driver is not None:
+        if self.driver:
             win_size = self.driver.get_window_size()
             y = win_size["height"] / 2
             start_x = randint(30, win_size["width"])
@@ -109,7 +114,7 @@ class IOSMonkey:
                 duration=1000)
 
     def save_screenshot(self, ss_path):
-        if self.driver is not None:
+        if self.driver:
             try:
                 pic_name = self.device_name+datetime.now().strftime('%y%m%d%H%M%S') + \
                     str(randint(1, 60)) + ".png"
@@ -118,35 +123,84 @@ class IOSMonkey:
             except IOError:
                 print("save screenshot failed.")
 
+    def close_driver(self):
+        self.driver.close()
+
+    def wait_element(self, time, element_by, element, msg):
+        res = WebDriverWait(self.driver, time). until(expected_conditions.presence_of_element_located((element_by, element)), msg)
+        return res
+
+    def find_element(self, key, value):
+        res = self.driver.find_element_by_ios_predicate("%s == '%s'" % (key, value))
+        if res:
+            return True
+        else:
+            return False
+
+
+# 邮件发送
+def send_email(content, title, recipients):
+    headers = {'content-type': 'application/json;charset=UTF-8'}
+    url = "http://10.255.242.62:9080/message/mail/send"
+    url_detail = url+"?content="+content+"&title="+title+"&recipients="+recipients
+    r = requests.post(url_detail, headers=headers)
+    print(r.status_code)
+
+
+def run_loop(times, start_time):
+    times = times + 1
+    if times <3:
+        try:
+            ios_monkey = IOSMonkey(device_name, bundle_id=bundleId, device_udid=udid)
+            while True:
+                pack_name = ios_monkey.find_element("name", "当当测试包")
+                if not pack_name:
+                    ios_monkey.close_driver()
+                    run_loop(times)
+                random_num = randint(1, 100)
+                if random_num < percentage_tap:
+                    ios_monkey.random_tap()
+                elif random_num <= (percentage_tap + percentage_swipe):
+                    ios_monkey.swipe_y()
+                else:
+                    ios_monkey.swipe_x()
+                end_time = datetime.now()
+                if (end_time - start_time).seconds >= run_time:
+                    break
+        except Exception as e:
+            print(e)
+            print('reloading...')
+            run_loop(times)
+
 
 def main():
-    ios_monkey = IOSMonkey(device_name, bundle_id=bundleId, device_udid=udid)
-    dp = ''
-    if screenshot:
-        dp = ios_monkey.mkdir()
-
     start_time = datetime.now()
-    print("begin.")
-    while True:
-        random_num = randint(1, 100)
-        if random_num < percentage_tap:
-            ios_monkey.random_tap()
-            if screenshot:
-                ios_monkey.save_screenshot(dp)
-        elif random_num <= (percentage_tap + percentage_swipe):
-            ios_monkey.swipe_y()
-            if screenshot:
-                ios_monkey.save_screenshot(dp)
-        else:
-            ios_monkey.swipe_x()
-            if screenshot:
-                ios_monkey.save_screenshot(dp)
-        end_time = datetime.now()
-        if (end_time - start_time).seconds >= run_time:
-            break
+    print("Begin at %s" % start_time)
+    log_path = get_log_file(udid, start_time)
+    times = 0
+    run_loop(times, start_time)
+    res_dict = analyse_log_file(udid, log_path)
+    print(res_dict)
+    content = '本次运行日志，统计结果如下：\n' + str(res_dict) + '\n日志文件：' + log_path
+    nums = list(res_dict.values())
+    if sum(nums) == 0:
+        title = 'IOS自动化结果：无异常'
+    else:
+        title = 'IOS自动化结果：有异常'
+    recipients = 'lizihao@dangdang.com'
+    send_email(content, title, recipients)
     print("end.")
 
 
 if __name__ == '__main__':
+    # start_time = datetime.now()
+    # print("Begin at %s" % start_time)
+    # log_path = get_log_file(udid, start_time)
+    # times = 0
+    # run_loop(times)
+    # print(analyse_log_file(udid, log_path))
+    # print("end.")
+    # ios_monkey = IOSMonkey(device_name, bundle_id=bundleId, device_udid=udid)
+    # res = ios_monkey.find_element('name', '当当测试包')
+    # print(res)
     main()
-
--- mydemos/IOS_monkey/get_ios_log.py
+++ mydemos/IOS_monkey/get_ios_log.py
@@ -5,10 +5,10 @@
 
 
 import threading
-import datetime
+from datetime import datetime
 import os
-import time
-from libimobiledevice import get_devices, get_device_name, get_device_type, get_device_version, get_log, get_screenshot
+import re
+from libimobiledevice import *
 
 KEYWORDS = ["ANR ", "NullPointerException", "CRASH", "Force Closed"]
 
@@ -49,17 +49,57 @@ def filter_keyword(device):
 
 
 def get_log_path(tag):
-    year = datetime.datetime.now().strftime('%Y')
-    month = datetime.datetime.now().strftime('%m')
-    day = datetime.datetime.now().strftime('%d')
+    year = datetime.now().strftime('%Y')
+    month = datetime.now().strftime('%m')
+    day = datetime.now().strftime('%d')
     path = os.path.join(tag, year, month, day)
     if not os.path.exists(path):
         os.makedirs(path)
     return path
 
 
+def get_log_file(device, time_str):
+    now_time_str = datetime.strftime(time_str, '%Y%m%d%H%M%S')
+    log_name = '.'.join((now_time_str, 'log'))
+    log_path = os.path.join(BASE_PATH, 'logs')
+    if not os.path.exists(log_path):
+        os.mkdir(log_path)
+    path = os.path.join(log_path, log_name)
+    device_name = get_device_name(device)
+    device_type = get_device_type(device)
+    device_version = get_device_version(device)
+    message = "设备名称：%s, 类型：%s, 系统版本：%s, UID:%s" % (device_name, device_type, device_version, device)
+    with open(path, 'w') as f:
+        f.write(message)
+    command = "idevicesyslog -u %s >> %s &" % (device, path)
+    res = os.system(command)
+    print('IOS app log path: ' + path)
+    return path
+
+
+def analyse_log_file(udid, log_path):
+    try:
+        command = "ps -ef | grep 'idevicesyslog -u %s' | awk '{print $2}' | xargs kill -9" % udid
+        os.popen(command)
+    except Exception as e:
+        pass
+
+    with open(log_path, 'r') as fb:
+        log_content = fb.read()
+
+    res_dict = dict()
+    for key in KEYWORDS:
+        res_dict[key] = len(re.findall(r'%s' % key, log_content))
+    return res_dict
+
+
 if __name__ == '__main__':
-    show_devices()
-    time.sleep(3)
-    filter_keywords()
+    # show_devices()
+    # time.sleep(3)
+    # filter_keywords()
+    udId = '871877d00ea5cf3f189a5eeeb1365babdbc9a3ad'
+    logpath = '/Users/xujuan/Downloads/log.log'
+    print(analyse_log_file(udId, logpath))
+
+
 
